bb4aa1de041636e9e1fc59db48a275bf1f077f84
joelaspe <joe.laspe@gmail.com>
2023-07-07 12:47:59 -0700

    Completed java version of calPoints

--- 682. Baseball Game/calPoints.java
+++ 682. Baseball Game/calPoints.java
@@ -1,33 +1,29 @@
-/**
- * @param {string[]} operations
- * @return {number}
- */
-var calPoints = function(operations) {
-    let scoreArray = [];
-    // generate the scoreArray based on operations input array
-    for(let i = 0; i < operations.length; i++) {
-        switch(operations[i]) {
-            case '+': {
-                scoreArray.push(scoreArray[scoreArray.length-2] + scoreArray[scoreArray.length-1]);
-                continue;
-            }
-            case 'C': {
-                scoreArray.pop();
-                continue;
-            }
-            case 'D': {
-                scoreArray.push(scoreArray[scoreArray.length-1] * 2);
-                continue;
-            }
-            default: {
-                scoreArray.push(parseInt(operations[i]));
+import java.util.ArrayList;
+
+class Solution {
+    public int calPoints(String[] operations) {
+        ArrayList<Integer> scoreArray = new ArrayList<Integer>();
+        for(int i = 0; i < operations.length; i++) {
+            switch(operations[i]) {
+                case "+":
+                    scoreArray.add(scoreArray.get(scoreArray.size() - 2) + scoreArray.get(scoreArray.size() - 1));
+                    break;
+                case "C":
+                    scoreArray.remove(scoreArray.size()- 1);
+                    break;
+                case "D":
+                    scoreArray.add(scoreArray.get(scoreArray.size() - 1) * 2);
+                    break;
+                default:
+                    scoreArray.add(Integer.parseInt(operations[i]));
             }
         }
-    }    
-    // loop through the scoreArray, add it up, will use reduce() in javascript, may need for loop in other languages. Check for empty array first, will use 
-    return scoreArray.length ? scoreArray.reduce((accumulator, currentValue) => accumulator + currentValue) : 0;
-};
-
+        int sum = 0;
+        for(int i = 0; i < scoreArray.size(); i++) {
+            sum += scoreArray.get(i);
+        }
+        return sum;
+    }
 /* Intuition: The problem wants us to create a record that is empty at the beginning of the game. We will store this in an array of numbers []. We will iterate through the input array and on each element check what the operation is. Since the constraints are that there will be at least 2 elements for a + operation and at least 1 element for both C and D operation, we will not have to worry about edge cases. The operations that occur will always deal with the last element of our score array so pop() and push() like operations will be sufficient. At the end, add up the scores and return them.
 
 Approach: Declare a array `scoreArray`. Loop through the input `operations` and using a switch block look at each element to determine the operation to perform. 
@@ -37,15 +33,21 @@ Complexity:
 -Time: We loop through the input array operations once, and the score array once which can have a maximum size of n so time complexity is $$O(2n)$$ -> $$O(n)$$
 -Space: The score array could be as large as the input array at the most, so space complexity is $$O(n)$$.  */
 
-const array1 = ["5","2","C","D","+"];
-const output1 = calPoints(array1);
-console.log(`Expecting 30 got ${output1}`);
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        
+        String[] array1 = {"5","2","C","D","+"};
+        int output1 = sol.calPoints(array1);
+        System.out.println("Expecting 30 got " + output1);
 
-const array2 = ["5","-2","4","C","D","9","+","+"];
-const output2 = calPoints(array2);
-console.log(`Expecting 27 got ${output2}`);
+        String[] array2 = {"5","-2","4","C","D","9","+","+"};
+        int output2 = sol.calPoints(array2);
+        System.out.println("Expecting 27 got " + output2);
 
-const array3 = ["1", "C"];
-const output3 = calPoints(array3);
-console.log(`Expecting 0 got ${output3}`);
+        String[] array3 = {"1", "C"};
+        int output3 = sol.calPoints(array3);
+        System.out.println("Expecting 0 got " + output3);
+    }
+}
 

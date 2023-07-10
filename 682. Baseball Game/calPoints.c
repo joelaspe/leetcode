bdb64955e0faf556fd931d439e824ce8aeb56bcf
joelaspe <joe.laspe@gmail.com>
2023-07-10 15:52:22 -0700

    Completed python version of calPoints

--- 682. Baseball Game/calPoints.c
+++ 682. Baseball Game/calPoints.c
@@ -33,14 +33,14 @@ int calPoints(char ** operations, int operationsSize){
 }
 
 
-/* Intuition: The problem wants us to create a record that is empty at the beginning of the game. We will store this in an array of numbers []. We will iterate through the input array and on each element check what the operation is. Since the constraints are that there will be at least 2 elements for a + operation and at least 1 element for both C and D operation, we will not have to worry about edge cases. The operations that occur will always deal with the last element of our score array so pop() and push() like operations will be sufficient. At the end, add up the scores and return them.
+/* Intuition: The problem wants us to create a record that is empty at the beginning of the game. We will store this in an array of numbers []. We will iterate through the input array and on each element check what the operation is. Since the constraints are that there will be at least 2 elements for a + operation and at least 1 element for both C and D operation, we will not have to worry about edge cases. C doesn't have nice array functions like javascript/java or stacks, so we'll just use a tracker to maintain how much space of the allocated array we are using. At the end, add up the scores and return them.
 
 Approach: Declare a array `scoreArray`. Loop through the input `ops` and using a switch block look at each element to determine the operation to perform. 
-If `+`, we will add `scoreArray[scorearray.length-2] + scoreArray[scoreArray.length-1]` and add it to the end of the `scoreArray`. If `C`, we will remove the last element in `scoreArray[i]`; If `D` we will take the previous score in `scoreArray[scoreArray.length-1]` and double it, adding it to the end of the array `scoreArray`. If it's anything else it is guaranteed to be a string representing an integer from the range -3*10^4 to 3*10^4. Add this number to the end of the scoreArray. When the loop iteration is complete, loop through the scoreArray and add it up, then return the final number. Note: All string numbers will need to be convered to integer first. 
+If `+`, we will add `scoreArray[scorearray.length-2] + scoreArray[scoreArray.length-1]` and add it to the end of the `scoreArray`. If `C`, we will remove the last element in `scoreArray[i]`; If `D` we will take the previous score in `scoreArray[scoreArray.length-1]` and double it, adding it to the end of the array `scoreArray`. If it's anything else it is guaranteed to be a string representing an integer from the range -3*10^4 to 3*10^4. Add this number to the end of the scoreArray. When the loop iteration is complete, loop through the scoreArray and add it up, then return the final number. Note: All string numbers will need to be convered to integer first, so we use strtol(). 
 
 Complexity: 
--Time: We loop through the input array operations once, and the score array once which can have a maximum size of n so time complexity is $$O(2n)$$ -> $$O(n)$$
--Space: The score array could be as large as the input array at the most, so space complexity is $$O(n)$$.  */
+-Time: We loop through the input array operations once, and the score array once which has a maximum size of n so time complexity is $$O(2n)$$ -> $$O(n)$$
+-Space: The score array is as large as the input array at the most, so space complexity is $$O(n)$$.  */
 
 int main() {
     char* array1[5] = {"5", "2", "C", "D", "+"};
--- 682. Baseball Game/calPoints.cpp
+++ 682. Baseball Game/calPoints.cpp
@@ -27,7 +27,7 @@ public:
 
 /* Intuition: The problem wants us to create a record that is empty at the beginning of the game. We will store this in an array of numbers []. We will iterate through the input array and on each element check what the operation is. Since the constraints are that there will be at least 2 elements for a + operation and at least 1 element for both C and D operation, we will not have to worry about edge cases. The operations that occur will always deal with the last element of our score array so pop() and push() like operations will be sufficient. At the end, add up the scores and return them.
 
-Approach: Declare a array `scoreArray`. Loop through the input `ops` and using a switch block look at each element to determine the operation to perform. 
+Approach: Declare a vector `scoreArray`. Loop through the input `operations` and using a switch block look at each element to determine the operation to perform. 
 If `+`, we will add `scoreArray[scorearray.length-2] + scoreArray[scoreArray.length-1]` and add it to the end of the `scoreArray`. If `C`, we will remove the last element in `scoreArray[i]`; If `D` we will take the previous score in `scoreArray[scoreArray.length-1]` and double it, adding it to the end of the array `scoreArray`. If it's anything else it is guaranteed to be a string representing an integer from the range -3*10^4 to 3*10^4. Add this number to the end of the scoreArray. When the loop iteration is complete, loop through the scoreArray and add it up, then return the final number. Note: All string numbers will need to be convered to integer first. 
 
 Complexity: 

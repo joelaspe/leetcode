88ce2fe0f1315b1bc6b184436e30bb7b77360bf1
joelaspe <joe.laspe@gmail.com>
2023-08-31 09:37:43 -0700

    Completed C++ version of 20. Valid Parentheses

--- 20. Valid Parentheses/isValid.cpp
+++ 20. Valid Parentheses/isValid.cpp
@@ -17,25 +17,36 @@ public:
             if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
                 recordStack.push(s[i]);
             } else {
+                //edge case, if stack is empty but we received a closing bracket, automatically return false
+                if(recordStack.empty()) {
+                    return false;
+                }
                 switch (s[i]) {
                     case ')': {
-                        if(recordStack.pop() != '(') {
+                        if(recordStack.top() != '(') {
                             return false;
                         }
                         break;
                     }
                     case ']': {
-                        if(recordStack.pop() != '[') {
+                        if(recordStack.top() != '[') {
                             return false;
                         }
                         break;
                     }
                     case '}': {
-                        if(recordStack.pop() != '{') {
+                        if(recordStack.top() != '{') {
                             return false;
                         }
+                        break;
+                    }
+                    default: {
+                        // received an invalid character, return false
+                        return false;
                     }
                 }
+                // have to use top() in the switch because pop() doesn't actually return the value when popped. Once we've done our logic, then remove the last element in the stack using pop()
+                recordStack.pop(); 
             }
         }
         if(recordStack.size() > 0) {
@@ -44,6 +55,15 @@ public:
         return true;
     }
 };
+
+/* Intuition: Given an input string that comprises only the characters '(', ')', '{', '}', '[' and ']' we are asked to determine if the input string is valid based on if the parentheses (bracket, curly or normal) are the appropriate type and in order. We need a way to store the opening bracket and the order each opening bracket appeared in. When we start seeing the closing brackets again, look at the most recent think added and see if it matches the expected. A stack structure would seem to work best for this purpose. 
+
+Approach:  Declare an array to store the record of opening brackets. Begin iterating through the input string, and check if it is an opening bracket, if it is, add that character into the next part of the recordArray. If instead a closing bracket is encountered, we compare the last added bracket in the recordArray and compare it to the closing bracket (i.e. if we have ']' then '[' should be on record) If at any time that comparison fails, return false. After iteration through the input string, do a final check of the tracker index, it should be at 0. If it is not, return false because we didn't fully close out the opening brackets. Otherwise return true.
+
+Complexity: 
+-Time: We loop through the input array operations once so the time complexity is $$O(n)$$
+-Space: The record array could be as large as the input array at the most, so space complexity is $$O(n)$$.  */
+
 
 int main() {
     Solution sol;
@@ -87,10 +107,3 @@ int main() {
 
 }
 
-/* Intuition: Given an input string that comprises only the characters '(', ')', '{', '}', '[' and ']' we are asked to determine if the input string is valid based on if the parentheses (bracket, curly or normal) are the appropriate type and in order. We need a way to store the opening bracket and the order each opening bracket appeared in. When we start seeing the closing brackets again, look at the most recent think added and see if it matches the expected. A stack structure would seem to work best for this purpose. 
-
-Approach:  Declare an array to store the record of opening brackets. Begin iterating through the input string, and check if it is an opening bracket, if it is, add that character into the next part of the recordArray. If instead a closing bracket is encountered, we compare the last added bracket in the recordArray and compare it to the closing bracket (i.e. if we have ']' then '[' should be on record) If at any time that comparison fails, return false. After iteration through the input string, do a final check of the tracker index, it should be at 0. If it is not, return false because we didn't fully close out the opening brackets. Otherwise return true.
-
-Complexity: 
--Time: We loop through the input array operations once so the time complexity is $$O(n)$$
--Space: The record array could be as large as the input array at the most, so space complexity is $$O(n)$$.  */
